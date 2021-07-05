0121ffec42432806cb9002ab6ce26533ff4b194a
Kanish2002 <35307799+Kanish2002@users.noreply.github.com>
2021-07-05 13:48:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ function compile() {
 	var css = document.getElementById("css");
 	var js = document.getElementById("js");
 	var code = document.getElementById("code").contentWindow.document;
-	let myCoolCode = document.createElement("script");
-    myCoolCode.setAttribute("src", "https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js");
-    document.body.appendChild(myCoolCode);
 	
 	 document.body.onclick = function(){
 	    	code.open();
