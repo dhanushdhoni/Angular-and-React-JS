b117c5129c8d21b6899063430b99c1b9a6bca0b3
Kanish2002 <35307799+Kanish2002@users.noreply.github.com>
2021-07-06 16:35:24 +0530

    Update editor.js

--- editor.js
+++ editor.js
@@ -5,7 +5,7 @@ function compile() {
 	var js = document.getElementById("js");
 	var rjs = document.getElementById("rjs");
 	var Ascr = "<script src='https://ajax.googleapis.com/ajax/libs/angularjs/1.8.2/angular.min.js'></script>";
-	//var Rscr = '<script src="https://unpkg.com/react@16/umd/react.production.min.js"></script><script src="https://unpkg.com/react-dom@16/umd/react-dom.production.min.js"></script><script src="https://unpkg.com/babel-standalone@6.15.0/babel.min.js"></script>';
+	
 	
 	var newIframe = document.createElement('iframe');
 	newIframe.setAttribute('id','iframe');
@@ -15,8 +15,8 @@ function compile() {
 	document.body.appendChild(newIframe);
 	
 	newIframe.contentWindow.document.open('text/html', 'replace');
-	newIframe.contentWindow.document.write('<script src="https://unpkg.com/react@16/umd/react.production.min.js"></script>\n'+'<script src="https://unpkg.com/react-dom@16/umd/react-dom.production.min.js"></script>'+'\n<script src="https://unpkg.com/babel-standalone@6.15.0/babel.min.js"></script>\n');
 	newIframe.contentWindow.document.write(html.value+"<style>"+css.value+"</style>"+"<script>" + js.value + "</script>"+"<script type = 'text/babel'>" + rjs.value + "</script>"+Ascr);
+	newIframe.contentWindow.document.write('<script src="https://unpkg.com/react@16/umd/react.production.min.js"></script>\n'+'<script src="https://unpkg.com/react-dom@16/umd/react-dom.production.min.js"></script>'+'\n<script src="https://unpkg.com/babel-standalone@6.15.0/babel.min.js"></script>\n');
 	newIframe.contentWindow.document.close();
     }
 function remove(){
