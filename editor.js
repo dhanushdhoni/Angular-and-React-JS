faea5531b289751ab9d5669c67d4a508752ff19b
Kanish2002 <35307799+Kanish2002@users.noreply.github.com>
2021-07-06 16:21:44 +0530

    Update editor.js

--- editor.js
+++ editor.js
@@ -14,7 +14,7 @@ function compile() {
 	document.body.appendChild(newIframe);
 	
 	newIframe.contentWindow.document.open('text/html', 'replace');
-	newIframe.contentWindow.document.write('<script src="https://unpkg.com/react@16/umd/react.production.min.js"></script><script src="https://unpkg.com/react-dom@16/umd/react-dom.production.min.js"></script><script src="https://unpkg.com/babel-standalone@6.15.0/babel.min.js"></script>');
+	newIframe.contentWindow.document.write('<script src="https://unpkg.com/react@16/umd/react.production.min.js"></script>\n'+'<script src="https://unpkg.com/react-dom@16/umd/react-dom.production.min.js"></script>'+'\n<script src="https://unpkg.com/babel-standalone@6.15.0/babel.min.js"></script>\n');
 	newIframe.contentWindow.document.write(html.value+"<style>"+css.value+"</style>"+"<script>" + js.value + "</script>"+Ascr);
 	newIframe.contentWindow.document.close();
     }
